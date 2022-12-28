a0fa1aca166a37d2889f13d0d08c47c9110363e0
SaidSultan1 <112623220+SaidSultan1@users.noreply.github.com>
2022-12-28 15:44:28 +0500

    Word count plugin

---
+++ our-first-unique-plugin.php
@@ -0,0 +1,8 @@
+<?php
+
+/*
+Plugin Name: WordCount Plugin
+Description: This plugin will help you to check how many word are in blog posts and how many time take you to read the full blog post
+Author Name: Said Sultan
+Author Uri:https://github.com/SaidSultan1/First-Plugin
+*/
